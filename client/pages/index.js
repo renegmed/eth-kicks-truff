7c190246ee5afe1a7efea1f0d6bcac879c857369
rene <renegmed@yahoo.com>
2018-08-10 10:31:24 -0400

    03 provided access to deployed campaigns

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
-//import { Card, Button } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 import Layout from '../components/Layout';
-//import { Link } from '../routes';
-//import getWeb3 from '../ethereum/getWeb3'
-//import getFactory from '../ethereum/getFactory'
-//import campaignFactoryDefinition from '../lib/contracts/CampaignFactory.json'
-//import Campaign from '../../contracts/Campaign.sol';
+import { Link } from '../routes'; ;
 import Web3Container from '../ethereum/Web3Container';
 
 
 class CampaignIndex extends Component {
+  state = {campaigns: null};
+ 
   async componentDidMount () {
     const { web3, campaignFactory } = this.props
-    console.log("====== campaignFactory ========");
-    console.log(campaignFactory);
-
+     
     const campaigns = await campaignFactory.methods.getDeployedCampaigns().call();
-    console.log("====== campaigns ========");
+    console.log("======  componentDidMount() campaigns ========");
     console.log(campaigns);
 
-    this.state = { campaigns };
+    this.setState({ campaigns });
+ 
   }
   
+  renderCampaigns() {
+    const campaigns = this.state.campaigns;
+    console.log("===== campaigns =======");
+    console.log(campaigns); 
+    
+    let items;
+    if (campaigns) {
+      items = campaigns.map(address => {
+        return {
+          header: address,
+          description: (
+            <Link >
+              <a>View Campaign</a>
+            </Link>
+          ),
+          fluid: true
+        };
+      });
+    }
+    return <Card.Group items={items} />;
+  }
+
   render() { 
-   
     return (
       <Layout>
         <div>
-          <h3>Open Campaigns</h3>
-
+          <h3>Open Campaigns</h3>  
+          {this.renderCampaigns()}
         </div>
       </Layout>
     );
